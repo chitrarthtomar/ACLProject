18aae709287e1a72abab7a15cb79839bb7bd07ee
unknown <aditi.giri@ADGI-BLR-01.accolite.com>
2018-01-22 14:01:25 +0530

    adding POJOS

--- acl.service/src/main/java/model/User.java
+++ acl.service/src/main/java/model/User.java
@@ -1,5 +1,66 @@
 package model;
 
+import java.util.List;
+
 public class User {
-	private String uI
+	private String uId;
+	private String uName;
+	private String uPassword;
+	private List<Group> uGroups;
+	private Object uMandatoryAttributes;
+	private Object uArbitraryAttributes;
+	private Object uResource;
+
+	public User(String uId, String uName,String uPassword, List<Group> uGroups, String uMandatoryAttributes, String uArbitraryAttributes,List<Object> uResource) {
+		this.uId = uId;
+		this.uName = uName;
+		this.uGroups = uGroups;
+		this.uMandatoryAttributes = uMandatoryAttributes;
+		this.uArbitraryAttributes = uArbitraryAttributes;
+		this.uPassword = uPassword;
+		this.uResource = uResource;
+	}
+
+	public String getuId() {
+		return uId;
+	}
+	public void setuId(String uId) {
+		this.uId = uId;
+	}
+	public String getuName() {
+		return uName;
+	}
+	public void setuName(String uName) {
+		this.uName = uName;
+	}
+	public List<Group> getuGroups() {
+		return uGroups;
+	}
+	public void setuGroups(List<Group> uGroups) {
+		this.uGroups = uGroups;
+	}
+	public Object getuMandatoryAttributes() {
+		return uMandatoryAttributes;
+	}
+	public void setuMandatoryAttributes(String uMandatoryAttributes) {
+		this.uMandatoryAttributes = uMandatoryAttributes;
+	}
+	public Object getuArbitraryAttributes() {
+		return uArbitraryAttributes;
+	}
+	public void setuArbitraryAttributes(String uArbitraryAttributes) {
+		this.uArbitraryAttributes = uArbitraryAttributes;
+	}
+	public String getuPassword() {
+		return uPassword;
+	}
+	public void setuPassword(String uPassword) {
+		this.uPassword = uPassword;
+	}
+	public List<Object> getuResource() {
+		return uResource;
+	}
+	public void setuResource(List<Object> uResource) {
+		this.uResource = uResource;
+	}
 }
