a41ce6d9df95cf748c5b7e65f00a415f4dc3a6d0
unknown <vignesh.b@VIGB-BLR-01.accolite.com>
2018-01-28 15:29:50 +0530

    linked navtabs

--- frontend/app/scripts/controllers/GroupCtrls/groupAddCtrl.js
+++ frontend/app/scripts/controllers/GroupCtrls/groupAddCtrl.js
@@ -6,6 +6,9 @@ angular.module('myApp').controller('groupAddCtrl', function($scope,resourceServi
   $scope.choices = [];
   $scope.permissions=[];
   
+  $scope.dropDown1 = [];
+  $scope.dropDown2 = [];
+
   resourceService.getResources().then(function(res){
     $scope.totalResources = res;
     $scope.res_added = $scope.totalResources.filter(function(element){
@@ -14,8 +17,15 @@ angular.module('myApp').controller('groupAddCtrl', function($scope,resourceServi
     })) return false;
     else return true;
     },$scope.permissions) 
+    console.log($scope.res_added);
+    $scope.res_added.forEach(element => {
+      $scope.dropDown1.push(element.rName);
+      $scope.dropDown2.push(element.rPermissions);
+    });console.log($scope.dropDown1);
+    console.log($scope.dropDown2);
+
   });
-  console.log($scope.res_added);
+  
  if($scope.totalResources){
   $scope.res_added = $scope.totalResources.filter(function(element){
     if(this.find(function(e){
@@ -23,22 +33,28 @@ angular.module('myApp').controller('groupAddCtrl', function($scope,resourceServi
   })) return false;
   else return true;
   },$scope.permissions) 
+ 
+  
   }
+
+
+
+
     $scope.arbAddNewChoice = function() {
     var newItemNoArb = $scope.arbitraryChoices.length+1;
     $scope.arbitraryChoices.push({});
   };
 
   
-  $scope.show_resources = [];
-   $scope.getPermissions = function(){
-	         var key = $scope.choice,\;
-        // Here you could actually go out and fetch the options for a server.
-        var myNewOptions = option2Options[key];
-        // Now set the options.
-        // If you got the results from a server, this would go in the callback
-        $scope.options2 = myNewOptions;
-   }
+  // $scope.show_resources = [];
+  //  $scope.getPermissions = function(){
+	//          var key = $scope.choic;
+  //       // Here you could actually go out and fetch the options for a server.
+  //       var myNewOptions = option2Options[key];
+  //       // Now set the options.
+  //       // If you got the results from a server, this would go in the callback
+  //       $scope.options2 = myNewOptions;
+  //  }
   $scope.arbRemoveChoice = function() {
     var lastItemArb = $scope.arbitraryChoices.length-1;
     $scope.arbitraryChoices.splice(lastItemArb);
