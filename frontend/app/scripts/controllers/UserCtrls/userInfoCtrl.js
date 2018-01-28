1d6f90c88dcfd621448f1180f08f4b756e752694
unknown <aditi.giri@ADGI-BLR-01.accolite.com>
2018-01-28 21:32:31 +0530

    adding components in admin user

--- frontend/app/scripts/controllers/UserCtrls/userInfoCtrl.js
+++ frontend/app/scripts/controllers/UserCtrls/userInfoCtrl.js
@@ -1,21 +1,39 @@
 
 angular.module('myApp').controller('userInfoCtrl',['$scope','getUsers','$stateParams','resourceService', function($scope,getUsers
   ,$stateParams,resourceService) {
-	
- 
+
     $scope.id = $stateParams.id;
     getUsers.getUserById($scope.id).then(function(user){
       console.log(user);
       $scope.user = user;
-      $scope.arbAttrList = user.uArbitraryAttributes;
-      $scope.permissions = user.uResource;
+      $scope.arbAttrList = JSON.parse($scope.user.uArbitraryAttributes);
+	  $scope.manAttrList = JSON.parse($scope.user.uMandatoryAttributes);
+      $scope.permissions = JSON.parse($scope.user.uResource);
     })
+	
+	  resourceService.getResources().then(function(res){
+    $scope.totalResources = res;
+    $scope.res_added = $scope.totalResources.filter(function(element){
+      if(this.find(function(e){
+        return element.id == e.id;
+    })) return false;
+    else return true;
+    },$scope.permissions) 
+  });
+  // $scope.choices = [];  
+  // $scope.res_dd = ["Mobile", "Office", "Home"];
+  // $scope.perm_dd = ["Use", "Enter", "Live"];
+  if($scope.totalResources){
+  $scope.res_added = $scope.totalResources.filter(function(element){
+    if(this.find(function(e){
+      return element.id == e.id;
+  })) return false;
+  else return true;
+  },$scope.permissions) 
+  }
     
-    $scope.totalResources = resourceService.getResources();  
-    $scope.choices = [];
-    
-    $scope.res_dd = ["Mobile", "Office", "Home"];
-    $scope.perm_dd = ["Use", "Enter", "Live"];
+   // $scope.totalResources = resourceService.getResources();  
+
 
     // $scope.res_added = $scope.totalResources.filter(function(element){
     //   if(this.find(function(e){
@@ -23,6 +41,8 @@ angular.module('myApp').controller('userInfoCtrl',['$scope','getUsers','$statePa
     //   })) return false;
     //   else return true;
     // },$scope.permissions);
+
+    $scope.choices = [];
 
     $scope.addNewChoice = function() {
       var newItemNo = $scope.choices.length+1;
