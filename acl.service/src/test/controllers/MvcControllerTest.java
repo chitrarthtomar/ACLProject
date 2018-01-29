42ef9883e0eae11ed3cac10a75e77d28f6726ebe
cheetah <chitrarth007@hotmail.com>
2018-01-29 08:53:33 +0530

    testing for dto added

--- acl.service/src/test/controllers/MvcControllerTest.java
+++ acl.service/src/test/controllers/MvcControllerTest.java
@@ -1,20 +1,21 @@
 package controllers;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import dto.LoginDto;
 import dto.LoginResponseDto;
 
 import static org.junit.Assert.assertEquals;
 import model.User;
 import services.UserService;
-import services.UserServiceImpl;
 
 
 public class MvcControllerTest {
 	
 	MvcController mvcController = new MvcController();
-
-	UserService userService = new UserServiceImpl();
+	@Autowired
+	UserService userService;
 
 	@Test
     public void is_login_working() throws Exception {
@@ -22,6 +23,7 @@ public class MvcControllerTest {
     	json.setUsername("abc");
     	json.setPassword("Hello");
         User user = new User("abc", "8b1a9953c4611296a827abf8c4784d7", "admin", "test", "asd", "asd");
+        boolean didcreate = userService.createUser("abc", "8b1a9953c4611296a827abf8c4784d7", "admin", "test", "asd", "asd");
         userService.authenticateUser("abc", "8b1a9953c4611296a827abf8c4784d7");
         LoginResponseDto user2 = mvcController.viewuser(json);
         assertEquals(user.getuName(), user2.getUser().getuName());
